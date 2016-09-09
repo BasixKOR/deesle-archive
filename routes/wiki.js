59d906b4ff307a02e5dbfeaa374884be75ebb9ad
Basix <jsrtyu1120@gmail.com>
2016-09-09 22:06:26 +0900

    대문으로 가는 부분 구현

--- routes/wiki.js
+++ routes/wiki.js
@@ -3,9 +3,10 @@
 const Hoek = require('hoek');
 const mongoose = require('mongoose');
 const Doc = require(`${__dirname}/../utils/schema/Doc`)
+const setting = require(`${__dirname}/../setting`)
 module.exports = {
     // GET / == 대문으로 연결시킨다.
     "root": function(req, reply) {
-        
+        reply('대문으로 가세요.').redirect().location(`w/${setting.frontPage}`)
     }
 }
