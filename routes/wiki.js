ddba8e67077991cf4a9a95b4bdfeee7258679564
Basix <jsrtyu1120@gmail.com>
2016-09-09 21:51:04 +0900

    Doc 스키마를 못 찾는 버그, Schema를 못 찾는 버그 수정

--- routes/wiki.js
+++ routes/wiki.js
@@ -2,7 +2,7 @@
 
 const Hoek = require('hoek');
 const mongoose = require('mongoose');
-const Doc = require(`${__dirname}/utils/schema/Doc`)
+const Doc = require(`${__dirname}/../utils/schema/Doc`)
 module.exports = {
     // GET / == 대문으로 연결시킨다.
     "root": function(req, reply) {
--- utils/schema/Doc.js
+++ utils/schema/Doc.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+var Schema = mongoose.Schema;
 var schema = new Schema({
   name: {
       type: String,
