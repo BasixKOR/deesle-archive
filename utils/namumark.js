b7ec08dc01fb2faa02101756788f03253d0a2139
Basix <jsrtyu1120@gmail.com>
2016-10-03 18:55:20 +0900

    파서 개발 시작

--- utils/namumark.js
+++ utils/namumark.js
@@ -7,27 +7,21 @@ module.exports = {
         const process = {
             "preprocess": function(d) {
                 var cd = d
+                cd.replace(/([\n]+)/g, "<p>$1</p>")
                 return cd
             },
             "blockmarkup": function(d) {
                 var cd = d
                 return cd
             },
-            "TOC": function(d) {
-                var cd // Array
-                return cd
-            },
             "markup": function(d) {
                 var cd = d
                 return cd
-            },
-            "compare": (d, TOC) => TOC.join('') + d
+            }
         }
 
         parsed = process.preprocess(nmText) // br 처리 등등
         parsed = process.blockmarkup(parsed) // 굵게 등의 간단한 문법
-        TOC = process.TOC(parsed) // 목차 생성
         parsed = process.markup(parsed) // 일부 복잡한 문법
-        parsed = process.compare(parsed, TOC)
     }
 }
