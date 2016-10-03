08ddfd4dd802610809bd3537b861558e24f70c45
Basix <jsrtyu1120@gmail.com>
2016-10-03 21:20:53 +0900

    setup 일부 문제 수정

--- routes/setup.js
+++ routes/setup.js
@@ -10,9 +10,13 @@ const setting = require(`${__dirname}/../setting`)
 module.exports = {
     // GET /setup/ == 설치 시작 페이지로 이동
     "root": function(request, reply) {
-        return reply.view('setup', {
-            pagename: "deesle 설치"
-        })
+        if(setting.needSetup) {
+            return reply.view('setup', {
+                pagename: "deesle 설치"
+            })
+        } else {
+            reply('이미 설치되었습니다.').redirect().location('')
+        }
     },
     "begin": function(request, reply) {
         let config = { needSetup: false }
@@ -25,15 +29,16 @@ module.exports = {
             admin: true
         });
 
-        config.mongoUrl = data.mongoUrl
-        config.frontPage = data.frontPage
-
         admin.save(function (err) {
             !err || console.error(err)
+
+            config.mongoUrl = data.mongoUrl
+            config.frontPage = data.frontPage 
+
+            jsonfile.writeFile(`${__dirname}/../setting`, config, {spaces: 2}, function(err) {
+                !err || console.error(err) // 에러가 있는 경우 출력
+                reply('설치되었습니다.').redirect().location('')
+            })
         });
- 
-        jsonfile.writeFile(`${__dirname}/../setting`, config, {spaces: 2}, function(err) {
-            !err || console.error(err) // 에러가 있는 경우 출력
-        })
     }
 }
