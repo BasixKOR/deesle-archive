7fc7653a9c1834d56c12706fb0dde651f7c1510b
Basix <jsrtyu1120@gmail.com>
2017-02-08 16:05:50 +0900

    토크나이저 추가 및 chai로 변경

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var expect = require('expect.js')
+var expect = require('chai').expect
 var util = require('../utils/util')
 const namumark = require(`../utils/namumark`)
 const authVaildate = require(`../utils/authVaildate`) // eslint-disable-line no-unused-vars
